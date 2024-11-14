32d0913480604e57a626156a6e5db19b8f337685
Aosaf098 <aosafibad10@gmail.com>
2024-11-14 18:09:30 +0600

    Auth Ongoing

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,25 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/Firebase/firebase.init";
+import { FiEyeOff, FiEye } from "react-icons/fi";
 
 const LogIn = ({ handleSignUpActive }) => {
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessmessage] = useState(false);
+  const [eyeOpen, setEyeOpen] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const logEmail = e.target.email.value;
+    const logPassword = e.target.password.value;
+    const logAcceptTerms = e.target.terms[1].checked;
+
+    setErrorMessage("");
+    setSuccessmessage(false);
+
+    if (!logAcceptTerms) {
+        setErrorMessage('Please Accept our terms & Conditions')
+        return
+    }
+
+    console.log(logEmail, logPassword, logAcceptTerms);
+
+    signInWithEmailAndPassword(auth, logEmail, logPassword)
+      .then((res) => {
+        console.log(res.user);
+        setSuccessmessage(true);
+      })
+      .catch((error) => {
+        console.log("ERROR", error.message);
+        setErrorMessage(error.message);
+        setSuccessmessage(false);
+      });
+  };
+
+  const passwordEye = () => {
+    setEyeOpen(!eyeOpen);
+  };
+
   return (
     <>
-      <form className="w-full flex flex-col items-center gap-10">
+      <form
+        onSubmit={handleLogin}
+        className="w-full flex flex-col items-center gap-10 relative"
+      >
         <h1 className="font-sora text-3xl lg:text-4xl">Log In</h1>
-        <Input
-          className="w-[90%] lg:w-3/5 font-sora font-bold"
-          type="email"
-          placeholder="Email"
-        />
-        <Input
-          className="w-[90%] lg:w-3/5 font-sora font-bold"
-          type="password"
-          placeholder="Password"
-        />
+        <div className="w-full flex flex-col items-center gap-8 relative">
+          <Input
+            className="w-[90%] lg:w-3/5 font-sora font-bold"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <Input
+            className="w-[90%] lg:w-3/5 font-sora font-bold"
+            type={eyeOpen ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+          />
+          <button
+            onClick={passwordEye}
+            className="absolute right-[120px] top-[78px]"
+          >
+            {eyeOpen ? <FiEye size={18} /> : <FiEyeOff size={18} />}
+          </button>
+        </div>
+        <div className="absolute top-[190px] left-[100px]">
+            <a href="#" className="text-[12px]">Forget Passowrd?</a>
+        </div>
+        <div className="flex items-center space-x-2 mt-4">
+          <Checkbox id="terms" name="terms" />
+          <label
+            htmlFor="terms"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Accept terms and conditions
+          </label>
+        </div>
+        {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
+        {successMessage && (
+          <p className="text-green-500 text-sm">Successfully Logged In</p>
+        )}
         <div>
           <Button>Continue</Button>
         </div>
+
         <p className="font-sora">
           Don't have an account?{" "}
           <a
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -3,23 +3,35 @@ import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { auth } from "@/Firebase/firebase.init";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FiEyeOff, FiEye } from "react-icons/fi"
+import { Checkbox } from "./ui/checkbox";
+
 
 const Register = ({ handleSignInActive }) => {
+
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessmessage] = useState(false);
+  const [eyeOpen, setEyeOpen] = useState(false)
 
   const handleRegister = (e) => {
     e.preventDefault();
     // const registerName = e.target.name.value
     const registerMail = e.target.email.value;
     const registerPassword = e.target.password.value;
+    const registerAcceptTerms = e.target.terms[1].checked
 
     setErrorMessage("");
     setSuccessmessage(false);
 
+    if (!registerAcceptTerms) {
+        setErrorMessage('Please Accept our terms & Conditions')
+        return
+    }
+
     createUserWithEmailAndPassword(auth, registerMail, registerPassword)
       .then((result) => {
         console.log(result.user);
+        console.log(registerAcceptTerms)
         setSuccessmessage(true);
       })
       .catch((error) => {
@@ -28,6 +40,8 @@ const Register = ({ handleSignInActive }) => {
         setSuccessmessage(false);
       });
   };
+
+ 
 
   const validatePassword = (password) => {
     const errors = [];
@@ -51,21 +65,26 @@ const Register = ({ handleSignInActive }) => {
     }
 
     if (errors.length > 0) {
-      errors.forEach((error) => {setErrorMessage(`- ${error}`)
-      console.log(`- ${error}`)
-    });
+      errors.forEach((error) => {
+        setErrorMessage(`- ${error}`);
+        console.log(`- ${error}`);
+      });
       return false;
     } else {
       return true;
     }
   };
 
+  const passwordEye = () => {
+    setEyeOpen(!eyeOpen)
+  }
+
   return (
     <>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          handleRegister;
+          handleRegister(e)
           validatePassword(e.target.password.value);
         }}
         className="w-full flex flex-col items-center gap-10"
@@ -77,21 +96,37 @@ const Register = ({ handleSignInActive }) => {
           type="name"
           placeholder="Username"
         /> */}
-        <Input
-          className="w-[90%] lg:w-3/5 font-sora font-bold"
-          name="email"
-          type="email"
-          placeholder="Email"
-        />
-        <Input
-          className="w-[90%] lg:w-3/5 font-sora font-bold"
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <div className="w-full flex flex-col items-center gap-8 relative">
+          <Input
+            className="w-[90%] lg:w-3/5 font-sora font-bold"
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <Input
+            className="w-[90%] lg:w-3/5 font-sora font-bold"
+            name="password"
+            type={eyeOpen ? 'text' : 'password'}
+            placeholder="Password"
+          />
+          <button onClick={passwordEye} className="absolute right-[120px] top-[78px]">
+            {
+                eyeOpen ? <FiEye size={18}/> : <FiEyeOff size={18} />
+            }
+            </button>
+        </div>
+        <div className="flex items-center space-x-2">
+            <Checkbox id="terms" name="terms" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Accept terms and conditions
+            </label>
+        </div>
+        {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
         {successMessage && (
-          <p className="text-green-500">Successfully Registered</p>
+          <p className="text-green-500 text-sm">Successfully Registered</p>
         )}
         <div>
           <Button>Continue</Button>
